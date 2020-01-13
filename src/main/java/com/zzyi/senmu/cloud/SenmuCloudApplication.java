f137353cd4c1e33c1cf7eb4455aa513345bbe5c8
zzyi <hezz@paraview.cn>
2020-01-13 10:19:13 +0800

    init project

---
+++ src/main/java/com/zzyi/senmu/cloud/SenmuCloudApplication.java
@@ -0,0 +1,13 @@
+package com.zzyi.senmu.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SenmuCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SenmuCloudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zzyi/senmu/cloud/SenmuCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zzyi.senmu.cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SenmuCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
